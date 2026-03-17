dc27b1986c8365fcc38b52ef35ea1a6ccc8f27a0
Vulong204 <Vulong281004@gmail.com>
2026-03-17 11:00:55 +0700

    commit note

---
+++ FirstWebMVC/Controllers/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using FirstWebMVC.Models.Entities;
+
+namespace FirstWebMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
